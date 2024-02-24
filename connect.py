4cfa7e4bd7a086daba3de47706777150960b8850
Chloe (Sijia) Hu <150668599+ChloeHu97@users.noreply.github.com>
2024-02-24 22:15:54 +1300

    add data

--- connect.py
+++ connect.py
@@ -1,7 +1,7 @@
 # These details are available on the first MySQL Workbench screen
 # Usually called 'Local Instance'
 dbuser = "root" #PUT YOUR MySQL username here - usually root
-dbpass = "1234" #PUT YOUR PASSWORD HERE
+dbpass = "16540789+" #PUT YOUR PASSWORD HERE
 dbhost = "localhost" 
 dbport = "3306"
-dbname = "pythonlogin"
+dbname = "biosecurity"
